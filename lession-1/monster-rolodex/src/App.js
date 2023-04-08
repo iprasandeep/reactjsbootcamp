fba87ecd1bb880b477c8231721dd5425693a4367
Prasandeep <prasandeepp@gmail.com>
2023-04-08 14:35:50 +0530

    state created!

--- lession-1/monster-rolodex/src/App.js
+++ lession-1/monster-rolodex/src/App.js
@@ -3,23 +3,29 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  
+
+  constructor(){
+    super();
+    this.state = {
+      name: {firstName: "Mohit", lastName:"Deep"},
+      company:"ZTM",
+    };
+  }
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi,{this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
+          <button onClick={()=>{
+          this.setState(()=>{
+            return {
+              name: {firstName: "Deep", lastName: 'Mohit'},
+            }
+          }, ()=>{
+            console.log(this.state);
+          })
+          }}>Change Name</button>
         </header>
       </div>
     );
